48fb4bec703e54c7503f3759780416caa10254fc
haesung.sean <haesung.sean@gmail.com>
2025-04-19 21:02:18 +0900

    노트북 수정

---
+++ mcp_sample/mcp_server.py
@@ -0,0 +1,18 @@
+from mcp.server.fastmcp import FastMCP
+from retriever import get_retriever, format_docs
+
+# FastMCP 초기화
+mcp = FastMCP(
+    "retrieve_ai_trends",
+    instructions="Retrieve the latest trends in AI. If your question is related to AI trends, use this tool. This tool also contains information about a person named Sean. The query must be in Korean.",
+    host="0.0.0.0",
+    port=4100,
+)
+
+
+@mcp.tool()
+async def retrieve_ai_trends(query: str) -> str:
+    """Retrieve the latest trends in AI. If your question is related to AI trends, use this tool. This tool also contains information about a person named Sean. The query must be in Korean."""
+    retriever = get_retriever()
+    docs = retriever.ainvoke(query)
+    return format_docs(docs)
---
+++ mcp_sample/retriever.py
@@ -0,0 +1,60 @@
+import os
+import warnings
+from dotenv import load_dotenv
+from pathlib import Path
+
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.document_loaders import PyMuPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
+
+BASE_DIR = Path(__file__).parents[1]
+ENV_PATH = BASE_DIR / ".env"
+
+load_dotenv(ENV_PATH)
+
+
+warnings.filterwarnings("ignore")
+
+
+DATA_PATH = Path("data")
+DB_PATH = Path("vector_store")
+PERSISTANT_DIRECTORY = DB_PATH
+
+os.makedirs(PERSISTANT_DIRECTORY, exist_ok=True)
+
+
+def get_retriever():
+    loader = PyMuPDFLoader(DATA_PATH / "ai_brief.pdf")
+    splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=200)
+
+    documents = loader.load()
+    chunks = splitter.split_documents(documents)
+
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+
+    if os.path.exists(PERSISTANT_DIRECTORY) and any(os.listdir(PERSISTANT_DIRECTORY)):
+        print(f"Loading existing vector store: {PERSISTANT_DIRECTORY}")
+        vector_store = Chroma(
+            persist_directory=str(PERSISTANT_DIRECTORY),
+            embedding_function=embeddings,
+            collection_name="ai_trends",
+        )
+    else:
+        vector_store = Chroma.from_documents(
+            documents=chunks,
+            embedding=embeddings,
+            collection_name="ai_trends",
+            persist_directory=str(PERSISTANT_DIRECTORY),
+        )
+
+    return vector_store.as_retriever(search_kwargs={"k": 3})
+
+
+def format_docs(docs):
+    result = []
+    for i, doc in enumerate(docs):
+        res = f"### 결과 {i+1}. Page : {doc.metadata['page']+1}\n"
+        res += doc.page_content
+        result.append(res)
+    return "\n\n=========================\n\n".join(result)
